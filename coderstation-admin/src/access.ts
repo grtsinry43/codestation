00ccd83d0e80b9a1d575c9f112c0e6136e9c1b9b
grtsinry43 <grtsinry43@outlook.com>
2024-10-05 13:39:11 +0800

    feat: 添加权限访问控制 为管理员和用户界面路由及操作按钮添加了基于角色的访问控制。区分了超级管理员（SuperAdmin）与普通管理员（NormalAdmin）的权限范围，确保用户只能访问其授权的功能。同时，在用户列表中增加了详情查看功能，并根据权限展示操作按钮。

--- coderstation-admin/src/access.ts
+++ coderstation-admin/src/access.ts
@@ -1,10 +1,22 @@
-export default (initialState: API.UserInfo) => {
+export default (
+  initialState: any,
+): {
+  SuperAdmin: boolean;
+  NormalAdmin: boolean;
+} => {
   // 在这里按照初始化数据定义项目中的权限，统一管理
   // 参考文档 https://umijs.org/docs/max/access
-  const canSeeAdmin = !!(
-    initialState && initialState.name !== 'dontHaveAccess'
-  );
-  return {
-    canSeeAdmin,
-  };
+  if (initialState) {
+    return {
+      SuperAdmin: initialState.adminInfo.permission === 1,
+      NormalAdmin:
+        initialState.adminInfo.permission === 1 ||
+        initialState.adminInfo.permission === 2,
+    };
+  } else {
+    return {
+      SuperAdmin: false,
+      NormalAdmin: false,
+    };
+  }
 };
--- coderstation-admin/src/pages/User/index.tsx
+++ coderstation-admin/src/pages/User/index.tsx
@@ -1,10 +1,13 @@
 import UserController from '@/services/user';
+import { formatDate } from '@/utils/tool';
+import { Access, useAccess } from '@@/exports';
 import { PageContainer, ProTable } from '@ant-design/pro-components';
 import { useNavigate } from '@umijs/max';
-import { Button, Image, message, Modal, Switch } from 'antd';
+import { Button, Image, message, Modal, Switch, Tag } from 'antd';
 import React, { useRef, useState } from 'react';
 
 const User: React.FC = () => {
+  const access = useAccess();
   const tableRef = useRef<any>();
   const [userList, setUserList] = useState([]);
   const navigate = useNavigate();
@@ -17,6 +20,19 @@ const User: React.FC = () => {
     pageSize: 10,
     total: 0,
   });
+
+  const [detailVisible, setDetailVisible] = useState(false);
+
+  const [detailInfo, setDetailInfo] = useState<any>({});
+
+  const showModal = (record: any) => {
+    setDetailVisible(true);
+    setDetailInfo(record);
+  };
+
+  const handleCancel = () => {
+    setDetailVisible(false);
+  };
 
   const deleteHandle = (id: string) => {
     Modal.confirm({
@@ -122,54 +138,127 @@ const User: React.FC = () => {
         <div className="action-container">
           <Button
             type="link"
+            onClick={() => showModal(record)}
+            key={`detail-${record._id}`}
+          >
+            详情
+          </Button>
+          <Button
+            type="link"
             onClick={() => navigate(`/user/edit/${record._id}`)}
             key={`edit-${record._id}`}
           >
             编辑
           </Button>
-          <Button
-            type="link"
-            onClick={() => deleteHandle(record._id)}
-            key={`delete-${record._id}`}
-          >
-            删除
-          </Button>
+          <Access accessible={access.SuperAdmin}>
+            <Button
+              type="link"
+              onClick={() => deleteHandle(record._id)}
+              key={`delete-${record._id}`}
+            >
+              删除
+            </Button>
+          </Access>
         </div>
       ),
     },
   ];
 
   return (
-    <PageContainer title={'用户列表'}>
-      <ProTable
-        headerTitle={'用户列表'}
-        dataSource={userList}
-        rowKey={'_id'}
-        actionRef={tableRef}
-        columns={columns}
-        pagination={{
-          ...pageInfo,
-          onChange: (current, pageSize) => {
-            setPageInfo({
-              current,
-              pageSize,
-              total: pageInfo.total,
+    <>
+      <PageContainer title={'用户列表'}>
+        <ProTable
+          headerTitle={'用户列表'}
+          dataSource={userList}
+          rowKey={'_id'}
+          actionRef={tableRef}
+          columns={columns}
+          pagination={{
+            ...pageInfo,
+            onChange: (current, pageSize) => {
+              setPageInfo({
+                current,
+                pageSize,
+                total: pageInfo.total,
+              });
+            },
+          }}
+          request={async (params: any) => {
+            const { data } = await UserController.getUserList({
+              ...params,
             });
-          },
-        }}
-        request={async (params: any) => {
-          const { data } = await UserController.getUserList({
-            ...params,
-          });
-          setUserList(data.data);
-          return {
-            data: data.data,
-            success: true,
-            total: data.count,
-          };
-        }}
-      ></ProTable>
-    </PageContainer>
+            setUserList(data.data);
+            return {
+              data: data.data,
+              success: true,
+              total: data.count,
+            };
+          }}
+        ></ProTable>
+      </PageContainer>
+      {/* 用户详情信息 */}
+      <Modal
+        title={detailInfo?.nickname}
+        open={detailVisible}
+        onCancel={handleCancel}
+        style={{ top: 60 }}
+        footer={false}
+      >
+        <h3> 登录账号 </h3>
+        <p>
+          <Tag color="red">{detailInfo?.loginId}</Tag>
+        </p>
+        <h3> 登录密码 </h3>
+        <p>
+          <Tag color="magenta">{detailInfo?.loginPwd}</Tag>
+        </p>
+        <h3> 当前头像 </h3>
+        <Image src={detailInfo?.avatar} width={60} />
+
+        <h3> 联系方式 </h3>
+        <div
+          style={{
+            display: 'flex',
+            width: '350px',
+            justifyContent: 'space-between',
+          }}
+        >
+          <div>
+            <h4>QQ</h4>
+            <p>{detailInfo?.qq ? detailInfo.qq : '未填写'}</p>
+          </div>
+          <div>
+            <h4> 微信 </h4>
+            <p>{detailInfo?.wechat ? detailInfo.weichat : '未填写'}</p>
+          </div>
+          <div>
+            <h4> 邮箱 </h4>
+            <p>{detailInfo?.mail ? detailInfo.mail : '未填写'}</p>
+          </div>
+        </div>
+        <h3> 个人简介 </h3>
+        <p>{detailInfo?.intro ? detailInfo.intro : '未填写'}</p>
+        <h3> 时间信息 </h3>
+        <div
+          style={{
+            display: 'flex',
+            width: '450px',
+            justifyContent: 'space-between',
+          }}
+        >
+          <div>
+            <h4> 注册时间 </h4>
+            <p>{formatDate(detailInfo?.registerDate)}</p>
+          </div>
+          <div>
+            <h4> 上次登录 </h4>
+            <p>{formatDate(detailInfo?.lastLoginDate)}</p>
+          </div>
+        </div>
+        <h3> 当前积分 </h3>
+        <p>{detailInfo?.points} 分 </p>
+      </Modal>
+    </>
   );
 };
 
